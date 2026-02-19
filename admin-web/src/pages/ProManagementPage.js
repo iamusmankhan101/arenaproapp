70c6d8604381d612bbe7b5834aea3e0eaec35aff
Usman Khan <imusmankhan10@gmail.com>
2026-02-19 12:48:51 +0500

    hjk

--- admin-web/src/pages/ProManagementPage.js
+++ admin-web/src/pages/ProManagementPage.js
@@ -58,7 +58,7 @@ export default function ProManagementPage() {
 
     useEffect(() => {
         loadVendors();
-    }, []);
+    }, [loadVendors]);
 
     const handleTogglePro = async () => {
         const { vendor, activate } = confirmDialog;
